62dcf1c2ff5b2ae4580711dd539c4485e4db9506
Tim Fletcher <timfletcher88@gmail.com>
2022-11-25 10:18:23 +1100

    Removed config delete, added warning instead

--- pantheon-deploy.sh
+++ pantheon-deploy.sh
@@ -24,11 +24,16 @@ if [[ $(echo $SITE) != '' ]] && $(cd $SITE); then
     cd $SITE
     echo -e 'Jumping to site location: '$SITE    
     if [[ $1 == 'dev' ]] || [[ $1 == 'test' ]] || [[ $1 == 'live' ]]; then
-       
-        echo -e 'Importing configuration to '$ENV' and rebuilding cache. You may need to run this several times if a memory error appears\n'
+        echo -e 'You may need to run this several times if a memory error appears, particularly when enabling modules\n'
+        
+        echo -e 'Importing new and updated configuration to '$ENV
         terminus remote:drush $PANTHEON_PROJECT.$ENV -- cim -y --partial && 
-       
-        echo -e 'Import complete, rebuilding cache' && 
+
+        echo -e 'cim --partial only imports new and updated config - deleted entities are left inact: \n\n
+        https://www.drush.org/latest/commands/config_import/\n\n
+        Delete these entities via the UI of the target site, where possible'
+        
+        echo -e 'Rebuilding cache' && 
         terminus remote:drush $PANTHEON_PROJECT.$ENV -- cr && 
         
         echo -e 'Cache rebuild complete!'
